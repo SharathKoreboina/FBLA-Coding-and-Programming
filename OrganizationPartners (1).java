df161e4c1745b0ad50c38d8470d436d87422c3dd
Sharath Koreboina <153569938+SharathKoreboina@users.noreply.github.com>
2023-12-11 21:03:01 -0500

    Add files via upload

---
+++ OrganizationPartners (1).java
@@ -0,0 +1,166 @@
+package src;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class OrganizationPartners {
+
+	public static ArrayList<String> additionalEntries = new ArrayList<String>();
+	 public static String[][] org1 = {{"Name of Organization: ", "IBM"},
+             {"Type of Organization: ", "Multinational technology company"},
+             {"Resources Availabe:   ", "Software overview, Appliances, Cloud services, Open source software. "},
+             {"Contact Information:  ", "1 (877) 426-6006"}};
+
+
+	 public static  String[][] org2 = {{"Name of Organization: ", "American Medical Association"},
+             {"Type of Organization: ", "National medical association"},
+             {"Resources Availabe:   ", "Resources and tools for physicians and wellness. "},
+             {"Contact Information:  ", "1 (800) 621-8335"}};
+
+
+	 public static  String[][] org3 = {{"Name of Organization: ", "Adobe"},
+             {"Type of Organization: ", "multinational computer software company"},
+             {"Resources Availabe:   ", "Premier Pro, Adobe AE, Photoshop, Creative Cloud. "},
+             {"Contact Information:  ", "1 (800) 833-6687"}};
+
+
+	 public static String[][] org4 = {{"Name of Organization: ", "Adobe"},
+             {"Type of Organization: ", "multinational computer software company"},
+             {"Resources Availabe:   ", "Premier Pro, Adobe AE, Photoshop, Creative Cloud. "},
+             {"Contact Information:  ", "1 (800) 833-6687"}};
+
+	 public static String[][] org5 = {{"Name of Organization: ", "IDEO"},
+             {"Type of Organization: ", "Non-profit design studio."},
+             {"Resources Availabe:   ", "Design thinking, IDEO U, OPENIDEO, THe D.School starter kit "},
+             {"Contact Information:  ", "hello@ideou.com"}};
+
+	 public static  String[][] org6 = {{"Name of Organization: ", "Xerox"},
+             {"Type of Organization: ", "American corporation"},
+             {"Resources Availabe:   ", "Scanners, printers, copy, email, and fax"},
+             {"Contact Information:  ", "1 (800) 835-6100"}};
+
+	 public static   String[][] org7 = {{"Name of Organization: ", "The National Fund"},
+             {"Type of Organization: ", "the dynamic national network of communities"},
+             {"Resources Availabe:   ", "Funds"},
+             {"Contact Information:  ", "Mail to 1250 Connecticut Ave. NW, Suite 200. Washington D.C."}};
+
+	 public static  String[][] org8 = {{"Name of Organization: ", "Tech Innovators Hub"},
+             {"Type of Organization: ", "Technology Incubator"},
+             {"Resources Availabe:   ", "State-of-the-art labs, mentorship programs, funding opportunities"},
+             {"Contact Information:  ", "(555) 123-4567"}};
+
+	 public static String[][] org9 = {{"Name of Organization: ", "Green Earth Foundation"},
+             {"Type of Organization: ", "Environmental Nonprofit"},
+             {"Resources Availabe:   ", "Conservation projects, eco-friendly education programs, volunteer opportunities"},
+             {"Contact Information:  ", "(555) 987-6543"}};
+
+	 public static String[][] org10 = {{"Name of Organization: ", "Global Health Initiative"},
+             {"Type of Organization: ", "Healthcare NGO"},
+             {"Resources Availabe:   ", "Medical clinics, vaccination programs, health "},
+             {"Contact Information:  ", "(555) 789-0123"}};
+
+	 public static String[][] org11 = {{"Name of Organization: ", "Artisans United"},
+             {"Type of Organization: ", "Creative Cooperative"},
+             {"Resources Availabe:   ", "Shared studio space, art supplies, marketing support"},
+             {"Contact Information:  ", "(555) 345-6789"}};
+
+	 public static String[][] org12 = {{"Name of Organization: ", "Future Scholars Foundation"},
+             {"Type of Organization: ", "Education Charity"},
+             {"Resources Availabe:   ", "Scholarships, educational materials, tutoring services"},
+             {"Contact Information:  ", "(555) 234-5678"}};
+
+	 public static String[][] org13 = {{"Name of Organization: ", "Community Builders Alliance"},
+             {"Type of Organization: ", "Community Development Organization"},
+             {"Resources Availabe:   ", "Affordable housing initiatives, job training programs, community events"},
+             {"Contact Information:  ", "(555) 876-5432"}};
+
+	 public static String[][] org14 = {{"Name of Organization: ", "Innovate for Good"},
+             {"Type of Organization: ", "Social Innovation Lab"},
+             {"Resources Availabe:   ", "Design thinking workshops, prototyping facilities, networking events"},
+             {"Contact Information:  ", "(555) 321-0987"}};
+
+	 public static String[][] org15 = {{"Name of Organization: ", "Culinary Care Collective"},
+             {"Type of Organization: ", "Food Assistance Network"},
+             {"Resources Availabe:   ", "Community kitchens, food distribution centers, cooking classes"},
+             {"Contact Information:  ", "(555) 567-8901"}};
+
+     public static String[][] org16 = {{"Name of Organization: ", "Elderly Empowerment Coalition"},
+             {"Type of Organization: ", "Senior Citizen Support Group"},
+             {"Resources Availabe:   ", "Senior centers, health checkup camps, companionship programs"},
+             {"Contact Information:  ", "(555) 678-9012"}};
+
+     public static String[][] org17 = {{"Name of Organization: ", "Space Explorers Foundation"},
+             {"Type of Organization: ", "Space Research Organization"},
+             {"Resources Availabe:   ", "Observatory, research grants, astronaut training programs"},
+             {"Contact Information:  ", "(555) 890-1234"}};
+
+     public static String[][] org18 = {{"Name of Organization: ", "Renewable Energy Coalition"},
+             {"Type of Organization: ", "Sustainable Energy Advocacy Group"},
+             {"Resources Availabe:   ", "Solar and wind energy projects, educational workshops, policy advocacy"},
+             {"Contact Information:  ", "(555) 123-7890"}};
+
+     public static String[][] org19 = {{"Name of Organization: ", "Youth Empowerment Network"},
+             {"Type of Organization: ", "Youth Development Organization"},
+             {"Resources Availabe:   ", "Leadership programs, career counseling, community service projects"},
+             {"Contact Information:  ", "(555) 789-0123"}};
+
+     public static String[][] org20 = {{"Name of Organization: ", "Digital Literacy Initiative"},
+             {"Type of Organization: ", "Technology Education Nonprofit"},
+             {"Resources Availabe:   ", "Coding classes, computer access programs, digital literacy campaigns"},
+             {"Contact Information:  ", "(555) 234-5678"}};
+
+     public static String[][] org21 = {{"Name of Organization: ", "Animal Guardians Society"},
+             {"Type of Organization: ", "Animal Welfare Organization"},
+             {"Resources Availabe:   ", "Animal shelters, veterinary services, adoption programs"},
+             {"Contact Information:  ", "(555) 345-6789"}};
+
+     public static String[][] org22 = {{"Name of Organization: ", "Peacebuilders Alliance"},
+             {"Type of Organization: ", "Conflict Resolution NGO"},
+             {"Resources Availabe:   ", "Mediation workshops, peacebuilding seminars, community dialogues"},
+             {"Contact Information:  ", "(555) 456-7890"}};
+
+     public static String[][] org23 = {{"Name of Organization: ", "Tech for Good Foundation"},
+             {"Type of Organization: ", "Social Tech Charity"},
+             {"Resources Availabe:   ", "Tech education for underserved communities, app development for social impact"},
+             {"Contact Information:  ", "(555) 567-8901"}};
+
+     public static String[][] org24 = {{"Name of Organization: ", "Clean Oceans Initiative"},
+             {"Type of Organization: ", "Environmental Conservation Group"},
+             {"Resources Availabe:   ", "Ocean cleanup expeditions, plastic recycling initiatives, marine conservation"},
+             {"Contact Information:  ", "(555) 678-9012"}};
+
+     public static String[][] org25 = {{"Name of Organization: ", "Rural Development Collaborative"},
+             {"Type of Organization: ", "Rural Community Support Network"},
+             {"Resources Availabe:   ", "Agricultural support, infrastructure development, community outreach"},
+             {"Contact Information:  ", "(555) 789-0123"}};
+    
+    public static void askUserForInfo() {
+    	Scanner input = new Scanner(System.in);
+    	String answer = "yes";
+    	do  {
+    			
+	           System.out.println("Provide the name of Organization: ");
+	           additionalEntries.add(input.nextLine());
+	            
+	           System.out.println("Type of organization: ");
+	           additionalEntries.add(input.nextLine());
+	            
+	           System.out.println("Provide resrouces available: ");
+	           additionalEntries.add(input.nextLine());
+
+	           System.out.println("Contact info: ");
+	           additionalEntries.add(input.nextLine());
+
+		      
+		       
+		       System.out.println("\n");
+	           System.out.println("Would you like to add another entry?");
+	           answer = input.nextLine();
+
+    	}while(answer.equalsIgnoreCase("yes"));
+
+
+    }
+    
+    
+}
